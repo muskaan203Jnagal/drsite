c24a49e4f4541278a5ee0b0159436ff5a4ca99a7
Palak Rahar <raharp25@gmail.com>
2026-03-28 18:20:16 +0530

    about page done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
+import 'about_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomePage(),
+      home: const AboutUsPage(),
     );
   }
 }
